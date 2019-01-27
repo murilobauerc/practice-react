a169253241535bcfbf0902e9333336085ecd20ee
murilobauerc <panchobauer@gmail.com>
2019-01-27 18:26:23 -0200

    delete button

--- src/CommentDetail.js
+++ src/CommentDetail.js
@@ -1,24 +1,28 @@
-import React from 'react';
-import faker from 'faker';
+import React from "react";
 
-const CommentDetail = (props) => {
-    console.log(props);
-        return (
-        <div className="comment">
-                <a href="/" className="avatar">
-                    <img alt="avatar" src={faker.image.avatar()} />
-                </a>
-                <div className="content">
-                    <a href="/" className="author">
-                        Sam
-                    </a>
-                    <div className="metadata">
-                        <span className="date">Today at 6:00PM</span>
-                    </div>
-                    <div className="text"> Nice blog post!</div>
-                </div>
-            </div>
-    );
+const CommentDetail = props => {
+  return (
+    <div className="comment">
+      <a href="/" className="avatar">
+        <img alt="avatar" src={props.src} />
+      </a>
+      <div className="content">
+        <a href="/" className="author">
+          {props.author}
+          <button onClick={event => props.handleDelete(event, props.id)}>
+            {" "}
+            Excluir{" "}
+          </button>
+        </a>
+        <div className="metadata">
+          <span className="date">{props.date}</span>
+        </div>
+        <div className="text">{props.text}</div>
+
+        <div className="text">{props.age}</div>
+      </div>
+    </div>
+  );
 };
 
 export default CommentDetail;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,62 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import faker from 'faker';
-import CommentDetail from './CommentDetail'; 
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import faker from "faker";
+import CommentDetail from "./CommentDetail";
 
-const App = () => {
+export default class App extends Component {
+  state = {
+    usuarios: [
+      {
+        author: "Veller",
+        date: new Date().getDate(),
+        text: "Delicious food.",
+        src: faker.image.avatar(),
+        age: faker.random.number(),
+        id: 1
+      },
+      {
+        author: "Murilo",
+        date: new Date().getDate(),
+        text: "Delicious food.",
+        src: faker.image.avatar(),
+        age: faker.random.number(),
+        id: 2
+      },
+      {
+        author: "Jean",
+        date: new Date().getDate(),
+        text: "Delicious food.",
+        src: faker.image.avatar(),
+        age: faker.random.number(),
+        id: 3
+      }
+    ]
+  };
+
+  handleDelete = (event, userToBeDeleted) => {
+    event.preventDefault();
+    this.setState({
+      usuarios: this.state.usuarios.filter(u => u.id !== userToBeDeleted)
+    });
+  };
+
+  render() {
     return (
-        <div className="ui container comments">
-            <CommentDetail author="Sam"/>
-            <CommentDetail author="Alex"/>
-            <CommentDetail author="Jane"/>
-        </div>  
+      <div className="ui container comments">
+        {this.state.usuarios.map(usuario => (
+          <CommentDetail
+            author={usuario.author}
+            date={usuario.date}
+            text={usuario.text}
+            src={usuario.src}
+            age={usuario.age}
+            id={usuario.id}
+            handleDelete={this.handleDelete}
+          />
+        ))}
+      </div>
     );
-};
+  }
+}
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
