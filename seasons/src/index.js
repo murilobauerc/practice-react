fc5437172b0e9eccbce3007257d9b7bb1f1b8adf
Murilo Rosa <murilobauerc@gmail.com>
2019-01-28 14:38:48 -0200

    adding another project Seasons

---
+++ seasons/src/index.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+
+export default class App extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = { lat: null}
+    }
+render(){
+    window.navigator.geolocation.getCurrentPosition(
+        (position) => console.log(position),
+        (err) => console.log(err) 
+    );
+    return <div>Latitude: </div>;
+    }
+}
+
+
+ReactDOM.render(
+    <App />,
+    document.querySelector("#root"));
+
