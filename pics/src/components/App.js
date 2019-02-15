7a9dee9f9a619524ba040d2c671771b302f73da8
Murilo Rosa <murilobauerc@gmail.com>
2019-02-15 14:26:31 -0200

    currently the page is useless, users can't do anything

---
+++ pics/src/components/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import SearchBar from "./SearchBar";
+
+export default class App extends Component {
+  onSearchSubmit(term) {
+    console.log(term);
+  }
+
+  render() {
+    return (
+      <div className="ui container" style={{ marginTop: "10px" }}>
+        {" "}
+        <SearchBar onSubmit={this.onSearchSubmit} />{" "}
+      </div>
+    );
+  }
+}
