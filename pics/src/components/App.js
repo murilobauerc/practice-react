a7565508ba8c198e790ccd8adaa734dba42aa029
Murilo Rosa <murilobauerc@gmail.com>
2019-02-22 14:52:18 -0300

    :sparkles:

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,16 +1,27 @@
 import React, { Component } from "react";
 import SearchBar from "./SearchBar";
+import youtube from "../apis/youtube";
+import VideoList from "./VideoList";
 
 export default class App extends Component {
-  onSearchSubmit(term) {
-    console.log(term);
-  }
+  state = { videos: [] };
+  onTermSubmit = async term => {
+    const response = await youtube.get("/search", {
+      params: {
+        q: term
+      }
+    });
+    this.setState({
+      videos: response.data.items
+    });
+    console.log(this.state.videos);
+  };
 
   render() {
     return (
-      <div className="ui container" style={{ marginTop: "10px" }}>
-        {" "}
-        <SearchBar onSubmit={this.onSearchSubmit} />{" "}
+      <div className="ui container">
+        <SearchBar onFormSubmit={this.onTermSubmit} />
+        <VideoList key={this.state.videos.id} videos={this.state.videos} />
       </div>
     );
   }
--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -3,24 +3,29 @@ import React, { Component } from "react";
 export default class SearchBar extends Component {
   state = { term: "" };
 
+  onInputChange = event => {
+    this.setState({
+      term: event.target.value
+    });
+  };
+
   onFormSubmit = event => {
     event.preventDefault();
-
-    this.props.onSubmit(this.state.term);
+    this.props.onFormSubmit(this.state.term);
+    // TODO : Make sure we call callback from parent component
   };
 
   render() {
     return (
-      <div className="ui segment">
+      <div className="search-bar ui segment">
         <form onSubmit={this.onFormSubmit} className="ui form">
           <div className="field">
-            <label> Image Search </label>
+            <label>Video Search</label>
             <input
               type="text"
+              placeholder="Search your favorite video"
               value={this.state.term}
-              onChange={event =>
-                this.setState({ term: event.target.value.toUpperCase() })
-              }
+              onChange={this.onInputChange}
             />
           </div>
         </form>
